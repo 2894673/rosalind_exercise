99aa5d9f831d4139b8e7dd114ed03421f9a7b2d1
Philipp Szechenyi <philipp.szechenyi@stud.fh-campuswien.ac.at>
2021-07-28 16:21:49 +0200

    fertig: DNA to RNA and reverse complimentary

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 class DRNA:
+    """[summary]
+    various tools for DNA/RNA string maniplulation
+    """
     def __init__(self, DRNA:str) -> str:
         self.DRNA = DRNA
+        
+    def print(self):
+        print(self.DRNA)
+        
     def count_nucleotides(self):
         A_count = 0
         C_count = 0
@@ -16,3 +23,11 @@ class DRNA:
             if i == "G":
                 G_count += 1
         return "A: {}, C: {}, G: {}, T: {}".format(A_count, C_count, T_count, G_count)
+    
+    def DNA_to_RNA(self):
+        return self.DRNA.replace("T", "U")
+    
+    def reverse_complement(self):
+        rev = self.DRNA[::-1]
+        rev_comp = rev.replace("A","t").replace("T","a").replace("G", "c").replace("C", "g").upper()
+        return rev_comp
